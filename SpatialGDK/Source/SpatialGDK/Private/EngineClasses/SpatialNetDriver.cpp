8657a78daaa7487f1590f65a23f39e6a31e5e982
Evi Kalogiannidou <evmorfiakalogiannidou@improbable.io>
2020-07-24 14:15:19 +0100

    Small fixes

--- SpatialGDK/Source/SpatialGDK/Private/EngineClasses/SpatialNetDriver.cpp
+++ SpatialGDK/Source/SpatialGDK/Private/EngineClasses/SpatialNetDriver.cpp
@@ -2443,7 +2443,7 @@ void USpatialNetDriver::RefreshActorDormancy(AActor* Actor, bool bMakeDormant)
 	}
 }
 
-void USpatialNetDriver::RefreshActorVisibility(AActor * Actor, bool bMakeVisible)
+void USpatialNetDriver::RefreshActorVisibility(AActor* Actor, bool bMakeVisible)
 {
 	check(IsServer());
 	check(Actor);
@@ -2451,7 +2451,7 @@ void USpatialNetDriver::RefreshActorVisibility(AActor * Actor, bool bMakeVisible
 	const Worker_EntityId EntityId = PackageMap->GetEntityIdFromObject(Actor);
 	if (EntityId == SpatialConstants::INVALID_ENTITY_ID)
 	{
-		UE_LOG(LogSpatialOSNetDriver, Verbose, TEXT("Unable to change visibility on actor (%s) without entity id"), *Actor->GetName());
+		UE_LOG(LogSpatialOSNetDriver, Verbose, TEXT("Unable to change visibility on an actor without entity id. Actor's Name: %s"), *Actor->GetName());
 		return;
 	}
 
@@ -2459,7 +2459,7 @@ void USpatialNetDriver::RefreshActorVisibility(AActor * Actor, bool bMakeVisible
 	const bool bHasAuthority = StaticComponentView->HasAuthority(EntityId, SpatialConstants::VISIBLE_COMPONENT_ID);
 	if (bHasAuthority == false)
 	{
-		UE_LOG(LogSpatialOSNetDriver, Warning, TEXT("Unable to  change visibility on (%s) without authority"), *Actor->GetName());
+		UE_LOG(LogSpatialOSNetDriver, Warning, TEXT("Unable to  change visibility on an actor without authority. Actor's Name: %s "), *Actor->GetName());
 		return;
 	}
 
